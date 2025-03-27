2fd6daa30b883b3db297500f8d2f163d5b1a3716
Rootpye <154875471+Rootpye@users.noreply.github.com>
2025-03-27 23:24:20 +0900

    background.js

--- voxscript-extension/background.js
+++ voxscript-extension/background.js
@@ -1,8 +1,8 @@
-chrome.action.onClicked.addListener((tab) => { // 또는 chrome.pageAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => { 
     chrome.windows.create({
       url: "popup.html",
-      type: "popup", // 또는 "normal"
-      width: 600, // 새 창의 너비 (선택 사항)
-      height: 400, // 새 창의 높이 (선택 사항)
+      type: "popup", 
+      width: 600, 
+      height: 400,
     });
   });
